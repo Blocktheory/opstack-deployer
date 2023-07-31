0e4c22661dca9f0f68cc5f0d4ecdcfe6d38eb1de
Chandher <chandher@designstring.com>
2023-07-31 11:25:52 +0530

    moving frontend code to repo

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>Making it seamless for users to be able to launch their own chain by using OP Stack</p>
+</div>
