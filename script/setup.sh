ddbbbdde1c66bfd984a85710c869c6d0b01e8d4c
Murugan D <yasmuru@users.noreply.github.com>
2023-08-01 16:00:39 +0530

    Update to fetch required params from .env file

--- script/setup.sh
+++ script/setup.sh
@@ -3,39 +3,36 @@
 # Enable "exit on error" and "pipefail" mode
 set -eo pipefail
 
-chain_name="blocktheory"
 output_location="/var/optimism/packages/contracts-bedrock/deploy-config/"
-eth_rpc_url=""
-admin_public_address=""
-admin_private_key=""
-sequencer_public_address=""
-sequencer_private_key=""
-batcher_public_address=""
-batcher_private_key=""
-proposer_public_address=""
-proposer_private_key=""
 
-
-while getopts ":n:i:a:A:s:S:b:B:p:P:" opt; do
-  case $opt in
-         n) chain_name="$OPTARG";;
-        i) chain_id="$OPTARG";;
-        a) admin_public_address="$OPTARG";;
-        A) admin_private_key="$OPTARG";;
-        s) sequencer_public_address="$OPTARG";;
-        S) sequencer_private_key="$OPTARG";;
-        b) batcher_public_address="$OPTARG";;
-        B) batcher_private_key="$OPTARG";;
-        p) proposer_public_address="$OPTARG";;
-        P) proposer_private_key="$OPTARG";;
-    \?) echo "Invalid option -$OPTARG" >&2
+# Function to check if a required parameter is missing
+function check_parameter {
+    if [ -z "$2" ]; then
+        echo "Error: $1 is not defined in the .env file."
         exit 1
-        ;;
-    :) echo "Option -$OPTARG requires an argument." >&2
-       exit 1
-       ;;
-  esac
-done
+    fi
+}
+
+# Load the .env file if it exists
+if [ -f .env ]; then
+    source .env
+else
+    echo "Error: .env file not found."
+    exit 1
+fi
+
+# Check required parameters
+check_parameter "CHAIN_NAME" "$CHAIN_NAME"
+check_parameter "CHAIN_ID" "$CHAIN_ID"
+check_parameter "ETH_RPC_URL" "$ETH_RPC_URL"
+check_parameter "ADMIN_PUBLIC_ADDRESS" "$ADMIN_PUBLIC_ADDRESS"
+check_parameter "ADMIN_PRIVATE_KEY" "$ADMIN_PRIVATE_KEY"
+check_parameter "SEQUENCER_PUBLIC_ADDRESS" "$SEQUENCER_PUBLIC_ADDRESS"
+check_parameter "SEQUENCER_PRIVATE_KEY" "$SEQUENCER_PRIVATE_KEY"
+check_parameter "BATCHER_PUBLIC_ADDRESS" "$BATCHER_PUBLIC_ADDRESS"
+check_parameter "BATCHER_PRIVATE_KEY" "$BATCHER_PUBLIC_ADDRESS"
+check_parameter "PROPOSER_PUBLIC_ADDRESS" "$PROPOSER_PUBLIC_ADDRESS"
+check_parameter "PROPOSER_PRIVATE_KEY" "$PROPOSER_PRIVATE_KEY"
 
 # Clone and install Optimism
 cd ~
@@ -56,7 +53,7 @@ make geth
 
 # Set ETH_RPC_URL environment variable
 cd ../optimism/
-export ETH_RPC_URL="$eth_rpc_url"
+export ETH_RPC_URL="$ETH_RPC_URL"
 
 
 output=$(cast block finalized --rpc-url $ETH_RPC_URL | grep -E "(timestamp|hash|number)")
@@ -69,37 +66,37 @@ json_data=$(cat <<EOF
 {
   "numDeployConfirmations": 1,
 
-  "finalSystemOwner": "$admin_public_address",
-  "portalGuardian": "$admin_public_address",
-  "controller": "$admin_public_address",
+  "finalSystemOwner": "$ADMIN_PUBLIC_ADDRESS",
+  "portalGuardian": "$ADMIN_PUBLIC_ADDRESS",
+  "controller": "$ADMIN_PUBLIC_ADDRESS",
 
   "l1StartingBlockTag": "$blockhash",
 
   "l1ChainID": 5,
-  "l2ChainID": 42069,
+  "l2ChainID": $CHAIN_ID,
   "l2BlockTime": 2,
 
   "maxSequencerDrift": 600,
   "sequencerWindowSize": 3600,
   "channelTimeout": 300,
 
-  "p2pSequencerAddress": "$sequencer_public_address",
-  "batchInboxAddress": "0xff00000000000000000000000000000000042069",
-  "batchSenderAddress": "$batcher_public_address",
+  "p2pSequencerAddress": "$SEQUENCER_PUBLIC_ADDRESS",
+  "batchInboxAddress": "0xff000000000000000000000000000000000$CHAIN_ID",
+  "batchSenderAddress": "$BATCHER_PUBLIC_ADDRESS",
 
   "l2OutputOracleSubmissionInterval": 120,
   "l2OutputOracleStartingBlockNumber": 0,
   "l2OutputOracleStartingTimestamp": $timestamp,
 
-  "l2OutputOracleProposer": "$proposer_public_address",
-  "l2OutputOracleChallenger": "$admin_public_address",
+  "l2OutputOracleProposer": "$PROPOSER_PUBLIC_ADDRESS",
+  "l2OutputOracleChallenger": "$ADMIN_PUBLIC_ADDRESS",
 
   "finalizationPeriodSeconds": 12,
 
-  "proxyAdminOwner": "$admin_public_address",
-  "baseFeeVaultRecipient": "$admin_public_address",
-  "l1FeeVaultRecipient": "$admin_public_address",
-  "sequencerFeeVaultRecipient": "$admin_public_address",
+  "proxyAdminOwner": "$ADMIN_PUBLIC_ADDRESS",
+  "baseFeeVaultRecipient": "$ADMIN_PUBLIC_ADDRESS",
+  "l1FeeVaultRecipient": "$ADMIN_PUBLIC_ADDRESS",
+  "sequencerFeeVaultRecipient": "$ADMIN_PUBLIC_ADDRESS",
 
   "baseFeeVaultMinimumWithdrawalAmount": "0x8ac7230489e80000",
   "l1FeeVaultMinimumWithdrawalAmount": "0x8ac7230489e80000",
@@ -114,7 +111,7 @@ json_data=$(cat <<EOF
   "enableGovernance": true,
   "governanceTokenSymbol": "OP",
   "governanceTokenName": "Optimism",
-  "governanceTokenOwner": "$admin_public_address",
+  "governanceTokenOwner": "$ADMIN_PUBLIC_ADDRESS",
 
   "l2GenesisBlockGasLimit": "0x1c9c380",
   "l2GenesisBlockBaseFeePerGas": "0x3b9aca00",
@@ -135,7 +132,7 @@ create_file_name_format() {
     echo "$formatted_string"
 }
 
-file_name_format=$(create_file_name_format "$chain_name")
+file_name_format=$(create_file_name_format "$CHAIN_NAME")
 
 echo "$json_data" > "$output_location/$file_name_format.json"
 
@@ -158,16 +155,16 @@ update_env_variable() {
     fi
 }
 
-update_env_variable "ETH_RPC_URL" "$eth_rpc_url"
+update_env_variable "ETH_RPC_URL" "$ETH_RPC_URL"
 update_env_variable "DEPLOYMENT_CONTEXT" "$file_name_format"
-update_env_variable "PRIVATE_KEY" "$admin_private_key"
-update_env_variable "L1_RPC" "$eth_rpc_url"
+update_env_variable "PRIVATE_KEY" "$ADMIN_PRIVATE_KEY"
+update_env_variable "L1_RPC" "$ETH_RPC_URL"
 update_env_variable "RPC_KIND" "alchemy"
 
-export ETH_RPC_URL=$eth_rpc_url
+export ETH_RPC_URL=$ETH_RPC_URL
 export DEPLOYMENT_CONTEXT=$file_name_format
-export PRIVATE_KEY=$admin_private_key
-export L1_RPC=$eth_rpc_url
+export PRIVATE_KEY=$ADMIN_PRIVATE_KEY
+export L1_RPC=$ETH_RPC_URL
 export RPC_KIND=alchemy
 
 source ~/.bashrc
@@ -186,7 +183,7 @@ go run cmd/main.go genesis l2 \
     --deployment-dir ../packages/contracts-bedrock/deployments/$file_name_format/ \
     --outfile.l2 genesis.json \
     --outfile.rollup rollup.json \
-    --l1-rpc $eth_rpc_url
+    --l1-rpc $ETH_RPC_URL
 
 # Set Up op-geth
 
@@ -205,25 +202,25 @@ direnv allow .
 
 # Run op-geth
 
-nohup ./build/bin/geth --datadir ./datadir --http --http.corsdomain="*" --http.vhosts="*" --http.addr=0.0.0.0 --http.api=web3,debug,eth,txpool,net,engine --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.origins="*" --ws.api=debug,eth,txpool,net,engine --syncmode=full --gcmode=archive --nodiscover --maxpeers=0 --networkid=42069 --authrpc.vhosts="*" --authrpc.addr=0.0.0.0 --authrpc.port=8551 --authrpc.jwtsecret=./jwt.txt --rollup.disabletxpoolgossip=true &
+nohup ./build/bin/geth --datadir ./datadir --http --http.corsdomain="*" --http.vhosts="*" --http.addr=0.0.0.0 --http.api=web3,debug,eth,txpool,net,engine --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.origins="*" --ws.api=debug,eth,txpool,net,engine --syncmode=full --gcmode=archive --nodiscover --maxpeers=0 --networkid=$CHAIN_ID --authrpc.vhosts="*" --authrpc.addr=0.0.0.0 --authrpc.port=8551 --authrpc.jwtsecret=./jwt.txt --rollup.disabletxpoolgossip=true &
 
 
 # Setting up op-node env
 cp -n .envrc ../optimism/op-node/
 cd ../optimism/op-node
 
-update_env_variable "SEQ_KEY" "$sequencer_private_key"
-update_env_variable "BATCHER_KEY" "$batcher_private_key"
+update_env_variable "SEQ_KEY" "$SEQUENCER_PRIVATE_KEY"
+update_env_variable "BATCHER_KEY" "$BATCHER_PRIVATE_KEY"
 
-export SEQ_KEY=$sequencer_private_key
-export BATCHER_KEY=$batcher_private_key
+export SEQ_KEY=$SEQUENCER_PRIVATE_KEY
+export BATCHER_KEY=$BATCHER_PRIVATE_KEY
 
 direnv allow .
 
 
 # Run op-node
 
-nohup ./bin/op-node 	--l2=http://localhost:8551 	--l2.jwt-secret=./jwt.txt 	--sequencer.enabled 	--sequencer.l1-confs=3 	--verifier.l1-confs=3 	--rollup.config=./rollup.json 	--rpc.addr=0.0.0.0 	--rpc.port=8547 	--p2p.disable 	--rpc.enable-admin 	--p2p.sequencer.key=$SEQ_KEY 	--l1=$eth_rpc_url 	--l1.rpckind=$RPC_KIND &
+nohup ./bin/op-node 	--l2=http://localhost:8551 	--l2.jwt-secret=./jwt.txt 	--sequencer.enabled 	--sequencer.l1-confs=3 	--verifier.l1-confs=3 	--rollup.config=./rollup.json 	--rpc.addr=0.0.0.0 	--rpc.port=8547 	--p2p.disable 	--rpc.enable-admin 	--p2p.sequencer.key=$SEQ_KEY 	--l1=$ETH_RPC_URL 	--l1.rpckind=$RPC_KIND &
 
 
 # setting up op-batcher
@@ -245,11 +242,11 @@ json_file="/var/optimism/packages/contracts-bedrock/deployments/$file_name_forma
 param_name="address"
 l2oo_addr=$(jq -r ".$param_name" "$json_file")
 
-update_env_variable "PROPOSER_KEY" "$proposer_private_key"
+update_env_variable "PROPOSER_KEY" "$PROPOSER_PRIVATE_KEY"
 update_env_variable "L2OO_ADDR" "$l2oo_addr"
 
 # sets Propose private key
-export PROPOSER_KEY=$proposer_private_key
+export PROPOSER_KEY=$PROPOSER_PRIVATE_KEY
 
 # sets address of L2OutputOracleProxy
 export L2OO_ADDR=$l2oo_addr
