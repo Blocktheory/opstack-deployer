479d1db873e1ae74ffaaeddf660a011ccc52dc54
Chandher <chandher@designstring.com>
2023-08-09 16:34:09 +0530

    Added the last step UI only

--- frontend/src/components/Setup.tsx
+++ frontend/src/components/Setup.tsx
@@ -38,12 +38,21 @@ const FooterButtons = ({ onNext, onBack, onReset, currStep }: any) => {
             &larr; Back
           </button>
         }
-        <button
-          onClick={onNext}
-          className="mx-2 rounded-full border bg-black hover:bg-white text-white hover:text-black font-semibold py-2 px-4 "
-        >
-          Next &rarr;
-        </button>
+        {currStep == 0 &&
+          <button
+            onClick={onNext}
+            className="mx-2 rounded-full border bg-black hover:bg-white text-white hover:text-black font-semibold py-2 px-4 "
+          >
+            Next &rarr;
+          </button>
+        }
+        {currStep != 0 &&
+          <button
+            className="mx-2 rounded-full border bg-black hover:bg-white text-white hover:text-black font-semibold py-2 px-4 "
+          >
+            Finish
+          </button>
+        }
       </span>
     </div>
   );
@@ -130,8 +139,158 @@ function Step1() {
 }
 
 
+
+
 function Step2() {
   return (
-    <></>
-  )
-}
+    <div className="mx-48">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-6 row-gap-10">
+          <div className="lg:py-6 lg:pr-16">
+            <div className="flex">
+              <div className="flex flex-col items-center mr-4">
+                <div>
+                  <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                    1
+                  </div>
+                </div>
+                <div className="w-px h-full bg-gray-300" />
+              </div>
+              <div className="pt-1 pb-8">
+                <p className="mb-2 text-lg font-bold">Hardware Requirments</p>
+                <p className="text-gray-700">
+                  <div className="card p-4">
+                    OS: Linux Ubuntu 20.04 LTS
+                    <br />RAM: Min. 8GB & 4 CPUs
+                    <br />
+                    Storage: 250GB or more
+                  </div>
+                  <p className="text-lg text-gray-700 font-semibold">
+                    You can use any cloud services like Digital Ocean, AWS,
+                    Google Cloud, etc.,
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="flex flex-col items-center mr-4">
+                <div>
+                  <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                    2
+                  </div>
+                </div>
+                <div className="w-px h-full bg-gray-300" />
+              </div>
+              <div className="pt-1 pb-8">
+                <p className="mb-2 text-lg font-bold">Clone the Repsistory</p>
+                <p className="text-gray-700">
+
+                  <div className="bg-black p-3 text-white">
+                    git clone https://github.com/Blocktheory/opstack-setup-script \n// navigate to the deploy script folder\ncd opstack-setup-script/deploy-scripts\n
+                  </div>
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="flex flex-col items-center mr-4">
+                <div>
+                  <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                    3
+                  </div>
+                </div>
+                <div className="w-px h-full bg-gray-300" />
+              </div>
+              <div className="pt-1 pb-8">
+                <p className="mb-2 text-lg font-bold">Run the Scritps</p>
+                <p className="text-gray-700">
+
+
+                  <div className="bg-black p-3 text-white">
+                    bash ./initial.sh\n or\n./initial.sh
+                  </div>
+
+                  <p className="text-md">
+                    Once initial.sh script runs successfully,
+                    <br />
+                    We can refresh the terminal using
+                    <code className="card p-2"> source ~/.bashrc</code>
+                    <br />
+                    Then we can run the setup / final script with below command
+                  </p>
+                  <div className="bg-black p-3 text-white">
+                    bash ./setup.sh\nor\n./setup.sh
+                  </div>
+                  <ul>
+                    <li>- Performs Initializes the OP Stack Starts</li>
+                    <li>- The OP Stack</li>
+                  </ul>
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="flex flex-col items-center mr-4">
+                <div>
+                  <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                    <svg
+                      className="w-4 text-gray-600"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <line
+                        fill="none"
+                        strokeMiterlimit="10"
+                        x1="12"
+                        y1="2"
+                        x2="12"
+                        y2="22"
+                      />
+                      <polyline
+                        fill="none"
+                        strokeMiterlimit="10"
+                        points="19,15 12,22 5,15"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div className="w-px h-full bg-gray-300" />
+              </div>
+              <div className="pt-1 pb-8">
+                <p className="mb-2 text-lg font-bold">L2 Chain Deployed Successfully</p>
+                <p className="text-gray-700">
+                  <p>
+                    Once the script runs successfully you can now see the
+                    new chain up and running in the port 8545. You can
+                    access it by using the RPC end points as <code
+                    >host:8454</code
+                    >
+                    along with chain id used earlier while running the script.
+                    The best way to submit feedback and report bugs is to
+                    <a
+                      href="https://github.com/Blocktheory/opstack-setup-script/issues"
+                      className="underline">open a GitHub issue</a
+                    ><br /> <br /> For more details refer:
+                    <a
+                      href="https://stack.optimism.io/#dive-deeper-into-the-op-stack"
+                      className="underline">OPStack Offical Docs</a
+                    >
+                    <br /> <br /> For setting up explorer, Please refer
+                    <a
+                      href="https://github.com/blocktheory/opstack-setup-script"
+                      className="underline">here</a
+                    >
+                  </p>
+
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
