39d1dbe2dd0b8b10f192396fd693aa7cf9ab1392
Chandher <chandher@designstring.com>
2023-08-09 15:35:46 +0530

    Basic UI setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { useAccount } from "wagmi";
 
 import { Attestooooooor } from "./components";
 
+import Image from "./assets/logo.png"
+import Setup from "./components/Setup";
+
 export function App() {
   /**
    * Wagmi hook for getting account information
@@ -12,18 +15,37 @@ export function App() {
 
   return (
     <>
-      <h1>OP Starter Project</h1>
+      <div className="flex p-3 mx-5 w-full justify-around">
 
-      {/** @see https://www.rainbowkit.com/docs/connect-button */}
-      <ConnectButton />
+        <img src={Image}></img>
 
-      {isConnected && (
-        <>
-          <hr />
-          <Attestooooooor />
-          <hr />
-        </>
-      )}
+        {/** @see https://www.rainbowkit.com/docs/connect-button */}
+        <ConnectButton />
+      </div>
+      <div className="bg-[#FEF7F8]">
+        {isConnected ? (
+          <>
+            <Setup />
+          </>
+        ) :
+          (
+            <>
+              <div className="flex justify-center items-center h-3/4">
+                <div className="text-center">
+                  <h1 className="text-3xl font-bold">Deploy your OP Stack Chain</h1>
+                  <p className="mt-2">Use the following interface to launch your L2 chain on OPStack. This interface can be used for testing and preparing for the super-chain, or you can modify it to suit your needs. Learn more</p>
+                </div>
+              </div>
+              <div>
+              </div>
+            </>
+
+          )}
+
+
+      </div>
     </>
   );
 }
+
+
