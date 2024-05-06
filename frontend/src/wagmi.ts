4d52f20e2145f196885e0ab5a57f76e7bfc603c6
Murugan D <yasmuru@users.noreply.github.com>
2024-05-06 17:59:58 +0530

    Update to support sepolia

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,5 +1,5 @@
 import { configureChains, createConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
+import { sepolia } from "wagmi/chains";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 // import { alchemyProvider } from "wagmi/providers/alchemy";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
@@ -10,7 +10,7 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
  * @see https://wagmi.sh/react/providers/configuring-chains
  */
 const { chains, publicClient } = configureChains(
-  [goerli],
+  [sepolia],
   [
     /**
      * Uncomment this line to use Alchemy as your provider
